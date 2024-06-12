d8443150f7aa37601c46c9ac94d58cd1a2599ac3
Rahul Singh <96829896+rahul12st@users.noreply.github.com>
2024-06-12 21:39:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from models import Model
 from utils import load_yaml_as_dict, save_file
+import base64
+import os
 
 # Load variables and paths
 VARIABLES = load_yaml_as_dict('variables/variables.yaml')
@@ -14,11 +16,18 @@ upload = st.file_uploader(
     'Upload a video or an image!',
     type=['mov', 'avi', 'mp4', 'mpg', 'mpeg', 'm4v', 'wmv', 'mkv']
 )
+
 if upload is not None:
     save_file(upload, PATHS['SOURCES'] + upload.name)
+
 if st.button('Start detecting'):
-    model.predict_video(
-        source=PATHS['SOURCES'] + upload.name,
-        target=PATHS['OUTPUTS'] + 'detected - ' + upload.name
-    )
+    processed_video_path = model.predict_video(source=PATHS['SOURCES'] + upload.name)
     
+    st.write("Video processing completed. Download the processed video:")
+    with open(processed_video_path, 'rb') as video_file:
+        video_bytes = video_file.read()
+        b64 = base64.b64encode(video_bytes).decode()
+        href = f'<a href="data:file/mp4;base64,{b64}" download="processed_video.mp4">Download Processed Video</a>'
+        st.markdown(href, unsafe_allow_html=True)
+    
+    os.remove(processed_video_path)  # Remove temporary processed video file
