9d0af97d01085e5d9db28f00538c415f51e606a0
Rahul Singh <96829896+rahul12st@users.noreply.github.com>
2024-06-13 00:04:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import tempfile
 import streamlit as st
 from models import Model
 from utils import load_yaml_as_dict, save_file
@@ -17,18 +16,18 @@ upload = st.file_uploader(
     'Upload a video or an image!',
     type=['mov', 'avi', 'mp4', 'mpg', 'mpeg', 'm4v', 'wmv', 'mkv']
 )
+
 if upload is not None:
-    # Use a temporary file for the uploaded video
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as tmpfile:
-        tmpfile.write(upload.getbuffer())
-        source_path = tmpfile.name
+    save_file(upload, PATHS['SOURCES'] + upload.name)
 
 if st.button('Start detecting'):
-    processed_video_path = model.predict_video(source=source_path)
-    st.write("Video processing completed. Download the processed video below.")
+    processed_video_path = model.predict_video(source=PATHS['SOURCES'] + upload.name)
     
+    st.write("Video processing completed. Download the processed video:")
     with open(processed_video_path, 'rb') as video_file:
         video_bytes = video_file.read()
         b64 = base64.b64encode(video_bytes).decode()
-        href = f'<a href="data:video/mp4;base64,{b64}" download="processed_video.mp4">Download processed video</a>'
+        href = f'<a href="data:file/mp4;base64,{b64}" download="processed_video.mp4">Download Processed Video</a>'
         st.markdown(href, unsafe_allow_html=True)
+    
+    os.remove(processed_video_path)  # Remove temporary processed video file
