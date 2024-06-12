c8b3751b7d227bc0eb734f7682813ad2ab88891b
Rahul Singh <96829896+rahul12st@users.noreply.github.com>
2024-06-13 01:22:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from models import Model
+from utils import load_yaml_as_dict, save_file
 import base64
+import os
+import tempfile
+# Load variables and paths
+VARIABLES = load_yaml_as_dict('variables/variables.yaml')
+PATHS = VARIABLES['PATHS']
 
 # Initialize the model with the 'best.pt' variant directly
 model = Model(variant='best.pt')
@@ -12,13 +18,19 @@ upload = st.file_uploader(
 )
 
 if upload is not None:
-    if st.button('Start detecting'):
-        # Save the uploaded file in memory
-        upload_bytes = upload.getvalue()
+    with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(upload.name)[1]) as tmpfile:
+        tmpfile.write(upload.getvalue())
+        upload_path = tmpfile.name
 
-        # Process the video directly from memory
-        processed_video_bytes = model.predict_video_from_bytes(upload_bytes)
+    if st.button('Start detecting'):
+        processed_video_path = model.predict_video(source=upload_path)
         
         st.write("Video processing completed. Download the processed video:")
-        href = f'<a href="data:file/mp4;base64,{base64.b64encode(processed_video_bytes).decode()}" download="processed_video.mp4">Download Processed Video</a>'
-        st.markdown(href, unsafe_allow_html=True)
+        with open(processed_video_path, 'rb') as video_file:
+            video_bytes = video_file.read()
+            b64 = base64.b64encode(video_bytes).decode()
+            href = f'<a href="data:file/mp4;base64,{b64}" download="processed_video.mp4">Download Processed Video</a>'
+            st.markdown(href, unsafe_allow_html=True)
+        
+        os.remove(processed_video_path)  # Remove temporary processed video file
+        os.remove(upload_path)  # Remove temporary upload file
